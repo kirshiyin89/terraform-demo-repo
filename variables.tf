8bed2a252f267d38ed333aff8df9e781e621a182
Kirshi Yin <70095657+kirshiyin89@users.noreply.github.com>
2025-04-13 14:33:43 +0200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "github_token" {
+  description = "GitHub personal access token"
+  sensitive   = true
+}
+
+variable "github_owner" {
+  description = "GitHub username or organization"
+}
+
