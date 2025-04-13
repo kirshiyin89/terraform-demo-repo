a0f99ccfa5344f469aa1a5173909e9544218d438
Kirshi Yin <70095657+kirshiyin89@users.noreply.github.com>
2025-04-13 14:38:57 +0200

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "github_token" {
-  description = "GitHub personal access token"
-  sensitive   = true
-}
-
-variable "github_owner" {
-  description = "GitHub username or organization"
-}
-
